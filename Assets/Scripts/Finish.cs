df64fc8c7acdd0036c56f3f3f5b6b39f4f9d3f90
elinaelkama <elina.elkama@hotmail.com>
2022-10-03 13:51:55 +0300

    ad goal + script

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            /*to delay 2s
+             Invoke("CompeleLevel", 2f)*/
+            CompleteLevel();
+        }
+    }
+
+    private void CompleteLevel() 
+    {
+        //reloads current
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        /*moves to next level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);*/
+    }
+}
