f64cfd2d13230ab8cace37f664cf4c796d6805fa
elinaelkama <elina.elkama@hotmail.com>
2022-10-03 12:16:02 +0300

    kill enemy

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -14,9 +14,15 @@ public class PlayerLife : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        GameObject player = GameObject.FindWithTag("Player");
+        GameObject enemy = GameObject.FindWithTag("Enemy");
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Die();
+            if (player.transform.position.y <= enemy.transform.position.y)
+            {
+                Die();
+            }
         }
     }
 
