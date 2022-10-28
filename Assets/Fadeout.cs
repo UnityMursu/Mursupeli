24b396e1d8d8c43f213bb7fd11f39298dd029bcd
Nallekari <tuomas.nallikari@outlook.com>
2022-10-28 17:31:15 +0300

    playercamera and fadeout scripts

---
+++ Assets/Fadeout.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fadeout : MonoBehaviour
+{
+    public float fadeOutTime = 1f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        GameObject hidden = GameObject.FindWithTag("Hidden");
+        
+    }
+    // Is called when something enters objects collider area
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FadeAlphaToZero(GetComponent<SpriteRenderer>(), fadeOutTime));
+        }
+    }
+
+    // slowly fadesout the object depeding on set time
+    IEnumerator FadeAlphaToZero(SpriteRenderer renderer, float duration) {
+        Color startColor = renderer.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0);
+        float time = 0;
+        while (time < duration) {
+            time += Time.deltaTime;
+            renderer.color = Color.Lerp(startColor, endColor, time/duration);
+            yield return null;
+        }
+    }
+
+    
+}
