583479ca0060547fe3103efd71f5fbe0c57140ca
Kemi <kembalos@hotmail.com>
2022-11-24 13:46:33 +0200

    ampumis äänet

--- Assets/Scripts/Ice.cs
+++ Assets/Scripts/Ice.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Ice : MonoBehaviour
 {
     public float speed = 20f;
+    public SpriteRenderer sprite;
     public Rigidbody2D rb;
+    public CircleCollider2D cc;
+    public bool destroyed;
+    [SerializeField] public AudioSource impact;
 
     // Start is called before the first frame update
     void Start ()
     {
         // Gives the ice projectile a velocity
+        sprite = GetComponent<SpriteRenderer>();
+        cc = GetComponent<CircleCollider2D>();
         rb.velocity = transform.right * speed;
+        destroyed = false;
     }
 
     void OnTriggerEnter2D (Collider2D info)
@@ -22,28 +29,45 @@ public class Ice : MonoBehaviour
         // Kill enemy when hit by an ice shot
         if (info.gameObject.CompareTag("Enemy"))
         {
+            impact.Play();
             Destroy (info.gameObject);
-            Destroy(gameObject);
+            sprite.enabled = false;
+            cc.enabled = false;
+            Destroy(gameObject, impact.clip.length);
         }
 
         // Destroy ice shot when it collides with a wall or the ground
         if (info.gameObject.CompareTag("Ground"))
         {
-            Destroy(gameObject);
+            impact.Play();
+            sprite.enabled = false;
+            cc.enabled = false;
+            Destroy(gameObject, impact.clip.length);
         }
 
         // Destroy ice shot when it collides with a trap
         if (info.gameObject.CompareTag("Trap"))
         {
-            Destroy(gameObject);
+            impact.Play();
+            sprite.enabled = false;
+            cc.enabled = false;
+            Destroy(gameObject, impact.clip.length);
         }
 
         // Destroy ice shot when it collides with a Pit
         if (info.gameObject.CompareTag("Pit"))
         {
-            Destroy(gameObject);
+            impact.Play();
+            sprite.enabled = false;
+            cc.enabled = false;
+            Destroy(gameObject, impact.clip.length);
         }
 
+    }
+
+    void OnDestroy()
+    {
+        destroyed = true;
     }
 
 }
